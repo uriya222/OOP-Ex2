e6584156d1570f7a79089880fcc8487497716449
ishay320 <ishay320@users.noreply.github.com>
2020-11-23 21:44:52 +0200

    typo

--- src/api/node_data.java
+++ src/api/node_data.java
@@ -11,7 +11,7 @@ public interface node_data {
 	 * Returns the key (id) associated with this node.
 	 * @return
 	 */
-	public int getkey();
+	public int getKey();
 	/** Returns the location of this node, if
 	 * none return null.
 	 * 
