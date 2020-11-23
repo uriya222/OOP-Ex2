73b919eccb4c779df046ee841bf747d85d3d874a
ishay320 <ishay320@users.noreply.github.com>
2020-11-23 16:06:51 +0200

    DWG

--- src/DWGraph_DS.java
+++
@@ -1,2 +0,0 @@
-public class DWGraph_DS{
-}
---
+++ src/api/DWGraph_DS.java
@@ -0,0 +1,137 @@
+package api;
+
+import java.util.Collection;
+
+public class DWGraph_DS implements directed_weighted_graph{
+    /**
+     * returns the node_data by the node_id,
+     *
+     * @param key - the node_id
+     * @return the node_data by the node_id, null if none.
+     */
+    @Override
+    public node_data getNode(int key){
+        return null;
+    }
+
+    /**
+     * returns the data of the edge (src,dest), null if none.
+     * Note: this method should run in O(1) time.
+     *
+     * @param src
+     * @param dest
+     * @return
+     */
+    @Override
+    public edge_data getEdge(int src, int dest){
+        return null;
+    }
+
+    /**
+     * adds a new node to the graph with the given node_data.
+     * Note: this method should run in O(1) time.
+     *
+     * @param n
+     */
+    @Override
+    public void addNode(node_data n){
+
+    }
+
+    /**
+     * Connects an edge with weight w between node src to node dest.
+     * * Note: this method should run in O(1) time.
+     *
+     * @param src  - the source of the edge.
+     * @param dest - the destination of the edge.
+     * @param w    - positive weight representing the cost (aka time, price, etc) between src-->dest.
+     */
+    @Override
+    public void connect(int src, int dest, double w){
+
+    }
+
+    /**
+     * This method returns a pointer (shallow copy) for the
+     * collection representing all the nodes in the graph.
+     * Note: this method should run in O(1) time.
+     *
+     * @return Collection<node_data>
+     */
+    @Override
+    public Collection<node_data> getV(){
+        return null;
+    }
+
+    /**
+     * This method returns a pointer (shallow copy) for the
+     * collection representing all the edges getting out of
+     * the given node (all the edges starting (source) at the given node).
+     * Note: this method should run in O(k) time, k being the collection size.
+     *
+     * @param node_id
+     * @return Collection<edge_data>
+     */
+    @Override
+    public Collection<edge_data> getE(int node_id){
+        return null;
+    }
+
+    /**
+     * Deletes the node (with the given ID) from the graph -
+     * and removes all edges which starts or ends at this node.
+     * This method should run in O(k), V.degree=k, as all the edges should be removed.
+     *
+     * @param key
+     * @return the data of the removed node (null if none).
+     */
+    @Override
+    public node_data removeNode(int key){
+        return null;
+    }
+
+    /**
+     * Deletes the edge from the graph,
+     * Note: this method should run in O(1) time.
+     *
+     * @param src
+     * @param dest
+     * @return the data of the removed edge (null if none).
+     */
+    @Override
+    public edge_data removeEdge(int src, int dest){
+        return null;
+    }
+
+    /**
+     * Returns the number of vertices (nodes) in the graph.
+     * Note: this method should run in O(1) time.
+     *
+     * @return
+     */
+    @Override
+    public int nodeSize(){
+        return 0;
+    }
+
+    /**
+     * Returns the number of edges (assume directional graph).
+     * Note: this method should run in O(1) time.
+     *
+     * @return
+     */
+    @Override
+    public int edgeSize(){
+        return 0;
+    }
+
+    /**
+     * Returns the Mode Count - for testing changes in the graph.
+     *
+     * @return
+     */
+    @Override
+    public int getMC(){
+        return 0;
+    }
+}
